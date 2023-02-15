ad57be187563fb5e447df613ed7e58b43518208f
HelenELee <HelenELee3@outlook.com>
2023-02-15 21:54:42 +0800

    added to questions

--- src/questions.js
+++ src/questions.js
@@ -1,5 +1,6 @@
 //use chalk to colour messages/error messages
 var chalk = require('chalk');
+let response = '';
 
 const welcomeQuestion = [
     {
@@ -17,7 +18,7 @@ const welcomeQuestion = [
 const cmsQuestions = [
     {
         type: 'list',
-        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "Quit"],
+        choices: ["Quit", "view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update employee role", "view utilized budget by dept"],
         message: "What would you like to do?",
         name: 'action',
          //this question is mandatory
@@ -32,8 +33,73 @@ const cmsQuestions = [
     }
 ]
 
+const dptQuestions = [
+    {
+        type: 'input',
+        message: 'What is the Department name?',
+        name: 'deptName',
+        //this question is mandatory
+        validate: deptName => {
+          if (deptName) {
+              return true;
+          } else {
+              console.log('Please provide a department name!');
+              return false;
+          }
+        }
+      }
+]
+
+const roleQuestions = `
+[{
+        type: 'input',
+        message: 'What is the Role name?',
+        name: 'roleName',
+        //this question is mandatory
+        validate: roleName => {
+          if (roleName) {
+              return true;
+          } else {
+              console.log('Please provide a role name!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        message: 'What is the salary?',
+        name: 'salary',
+        //this question is mandatory
+        validate: salary => {
+          if (salary) {
+              return true;
+          } else {
+              console.log('Please provide a salary!');
+              return false;
+          }
+        }
+      },
+      {
+        type: 'list',
+        choices: ${response},
+        message: 'What is the Department for this employee?',
+        name: 'deptID',
+        //this question is mandatory
+        validate: deptID => {
+          if (deptID) {
+              return true;
+          } else {
+              console.log('Please provide a department id!');
+              return false;
+          }
+        }
+      }]
+`
+
 //export so can be used in index.js with Inquirer
 module.exports = {
    welcomeQuestion : welcomeQuestion,
-   cmsQuestions : cmsQuestions 
+   cmsQuestions : cmsQuestions,
+   dptQuestions : dptQuestions,
+   roleQuestions : roleQuestions
 }
